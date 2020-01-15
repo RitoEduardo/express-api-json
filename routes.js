df5e33248804bb338756654cea5cf688cba5bc90
Eduardo Garcia <rito.garcia@ingeniat.com>
2020-01-14 18:56:10 -0600

    Se agrego servico de workload

--- routes.js
+++ routes.js
@@ -115,7 +115,6 @@ router.post('/users', function (req, res) {
   };
 
   const _user = new User();
-  //_user.initModel(data.firstName, data.lastName, data.age, data.mail );
   _user.initModel(data);
   res.json({
     response: true,
@@ -126,5 +125,125 @@ router.post('/users', function (req, res) {
 
 });
 
+const WorkloadJSON = require('./data/workload.json')
+const LoadDBJSON = require('./data/load.json')
+
+//Carga Horaria
+router.get('/workload', (req, res) => {
+
+  res.json({
+    response: true,
+    errors: [],
+    message: 'succesful',
+    data: WorkloadJSON
+  });
+
+});
+
+const days = [
+  {
+    "id": 1,
+    "name": "lunes"
+  },
+  {
+    "id": 2,
+    "name": "martes"
+  },
+  {
+    "id": 3,
+    "name": "miercoles"
+  },
+  {
+    "id": 4,
+    "name": "jueves"
+  },
+  {
+    "id": 5,
+    "name": "viernes"
+  },
+  {
+    "id": 6,
+    "name": "sabado"
+  }
+];
+
+const hours = [
+  {
+    "id": 1,
+    "name": "Matutino 1"
+  },
+  {
+    "id": 2,
+    "name": "Matutino 2"
+  },
+  {
+    "id": 3,
+    "name": "Matutino 3"
+  },
+  {
+    "id": 4,
+    "name": "Matutino 4"
+  },
+  {
+    "id": 5,
+    "name": "Matutino 5"
+  },
+  {
+    "id": 6,
+    "name": "Matutino 6"
+  },
+  {
+    "id": 7,
+    "name": "Matutino 7"
+  },
+  {
+    "id": 8,
+    "name": "Matutino 8"
+  },
+  {
+    "id": 9,
+    "name": "Matutino 9"
+  }
+];
+
+router.get('/workload/:curp', (req, res) => {
+
+  const curp = req.params.curp;
+  const workloads = WorkloadJSON.filter( r => r.CURP == curp );
+
+  if(!workloads || workloads.length == 0){
+    return res.json({
+      response: false,
+      errors: [{
+        "error": 1,
+        "language": "es",
+        "message": "Carga Horaria no disponible"
+      }],
+      message: "workload empty",
+      data: null
+    });
+  }
+
+  workloads.map( data => {
+    //delete data.id;
+    const temp = LoadDBJSON.find( r => r.id === data.id_carga );
+    delete data.id_carga
+    data['carga'] = temp;
+    const day = days.find( r => r.id === data.id_dia );
+    delete data.id_dia;
+    data['dia'] = day;
+    const hour = hours.find( r => r.id === data.id_hora );
+    delete data.id_hora;
+    data['hora'] = hour;
+  });
+
+  res.json({
+    response: true,
+    errors: [],
+    message: 'succesful',
+    data: workloads
+  });
+
+});
 
 module.exports = router;
